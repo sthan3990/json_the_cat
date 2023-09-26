a107af084ee199a6915784cdffbd99d3a09c6580
phankons <stephen.han@gmx.com>
2023-09-26 13:06:36 -0400

    Create prompt and fetching data

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,24 @@
-fd
+const request = require('request');
+
+const fetchBreedDescription = function(breedName, callback) {
+  try {
+    request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
+      if (err || response.statusCode !== 200) {
+        throw err || 'ERROR';
+      } else {
+        const data = JSON.parse(body);
+        if (data.length < 1) {
+          throw 'Breed not found.';
+        } else {
+          callback(null, data[0].description);
+        }
+      }
+    });
+  } catch (error) {
+    callback(error, null);
+  }
+};
+
+module.exports = {
+  fetchBreedDescription
+};
